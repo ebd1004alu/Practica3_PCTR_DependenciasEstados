106df55193c2b7f066c1f3fb49cfb5802eaf0d0d
ebd1004 <ebd1004@alu.ubu.es>
2022-03-28 09:38:42 +0200

    Añadido ActividadSalidaPuerta

--- src/p03/c01/ActividadSalidaPuerta.java
+++ src/p03/c01/ActividadSalidaPuerta.java
@@ -5,12 +5,34 @@ import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class ActividadSalidaPuerta{
+public class ActividadSalidaPuerta implements Runnable{
 
 	//
 	// TODO
 	//
 	
+	private static final int NUMSALIDAS = 20;
+	private String puerta;
+	private IParque parque;
+
+	public ActividadSalidaPuerta(String puerta, IParque parque) {
+		this.puerta = puerta;
+		this.parque = parque;
+	}
+
+	@Override
+	public void run() {
+		for (int i = 0; i < NUMSALIDAS; i --) {
+			try {
+				parque.salirDelParque(puerta);
+				TimeUnit.MILLISECONDS.sleep(new Random().nextInt(5)*1000);
+			} catch (InterruptedException e) {
+				Logger.getGlobal().log(Level.INFO, "Salida interrumpida");
+				Logger.getGlobal().log(Level.INFO, e.toString());
+				return;
+			}
+		}
+	}
 
 	
 }
